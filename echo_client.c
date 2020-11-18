c1ef866eb043fec1fff475f6f714161f290ffea1
omar-ahmad <omarahmad1998@gmail.com>
2020-11-18 15:14:48 -0500

    COE 768 Lab 4

---
+++ echo_client.c
@@ -0,0 +1,89 @@
+/* A simple echo client using TCP */
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+
+
+#define SERVER_TCP_PORT 3000	/* well-known port */
+#define BUFLEN		100	/* buffer length */
+
+int main(int argc, char **argv)
+{
+	int 	n, i, bytes_to_read;
+	int 	sd, port;
+	struct	hostent		*hp;
+	struct	sockaddr_in server;
+	char	*host, *bp, rbuf[BUFLEN], sbuf[BUFLEN];
+
+	switch(argc){
+	case 2:
+		host = argv[1];
+		port = SERVER_TCP_PORT;
+		break;
+	case 3:
+		host = argv[1];
+		port = atoi(argv[2]);
+		break;
+	default:
+		fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Create a stream socket	*/	
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "Can't creat a socket\n");
+		exit(1);
+	}
+
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if (hp = gethostbyname(host)) 
+	  bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+	else if ( inet_aton(host, (struct in_addr *) &server.sin_addr) ){
+	  fprintf(stderr, "Can't get server's address\n");
+	  exit(1);
+	}
+
+	/* Connecting to the server */
+	if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+	  fprintf(stderr, "Can't connect \n");
+	  exit(1);
+	}
+	
+	FILE *fp;
+	char tester[100] = "t";
+	
+	// scan for filename you wish to download
+	printf("Download: ");
+	scanf("%s", tester);
+	
+	// write(filename)
+	write(sd, &tester, BUFLEN);
+	
+	if(n=read(sd, rbuf, BUFLEN) == NULL)
+	{
+		printf("File '%s' does not exist.\n", tester);
+		close(sd);
+		return(0);
+	}
+	
+	fp = fopen(tester, "w");
+	
+	printf("Downloading: %s\n", tester);
+	while(n=read(sd, rbuf, BUFLEN) > 0){
+	  fprintf(fp, "%s", rbuf);
+	}
+
+	close(sd);
+	fclose(fp);
+	printf("Downloaded: %s\n", tester);
+	return(0);
+}
---
+++ echo_server.c
@@ -0,0 +1,120 @@
+/* A simple echo server using TCP */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+
+#define SERVER_TCP_PORT 3000	/* well-known port */
+#define BUFLEN		100	/* buffer length */
+
+int echod(int);
+void reaper(int);
+
+int main(int argc, char **argv)
+{
+	int 	sd, new_sd, client_len, port;
+	struct	sockaddr_in server, client;
+
+	switch(argc){
+	case 1:
+		port = SERVER_TCP_PORT;
+		break;
+	case 2:
+		port = atoi(argv[1]);
+		break;
+	default:
+		fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Create a stream socket	*/	
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "Can't creat a socket\n");
+		exit(1);
+	}
+
+	/* Bind an address to the socket	*/
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+		fprintf(stderr, "Can't bind name to socket\n");
+		exit(1);
+	}
+
+	/* queue up to 5 connect requests  */
+	listen(sd, 5);
+
+	(void) signal(SIGCHLD, reaper);
+
+	while(1) {
+	  client_len = sizeof(client);
+	  new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
+	  if(new_sd < 0){
+	    fprintf(stderr, "Can't accept client \n");
+	    exit(1);
+	  }
+	  switch (fork()){
+	  case 0:		/* child */
+		(void) close(sd);
+		exit(echod(new_sd));
+	  default:		/* parent */
+		(void) close(new_sd);
+		break;
+	  case -1:
+		fprintf(stderr, "fork: error\n");
+	  }
+	}
+}
+
+/*	echod program	*/
+int echod(int sd)
+{
+	char	*bp, buf[BUFLEN];
+	int 	n, bytes_to_read;
+	FILE *fp;
+	char str[1000];
+	char* filename;
+
+	// return from read, filename = filename inputted by client
+	read(sd, filename, BUFLEN); // need to read to get the file name
+	
+	// try to open file, if NULL, file doesn't exist
+	fp = fopen(filename, "r"); // open the file
+	if(fp == NULL)
+	{
+		fclose(fp);
+		write(sd, "File does not exist.\n", BUFLEN);
+		close(sd);
+		return(0);
+	}
+	else
+	{
+		write(sd, "", BUFLEN);
+	}
+	
+	// start uploading file to client
+	printf("Uploading: %s \n", filename);
+	while(fgets(buf, BUFLEN, fp) != NULL) // get file until file ends
+	{
+		write(sd, buf, BUFLEN);
+	}
+	
+	close(sd);
+	fclose(fp); // close the file
+	return(0);
+}
+
+/*	reaper		*/
+void	reaper(int sig)
+{
+	int	status;
+	while(wait3(&status, WNOHANG, (struct rusage *)0) >= 0);
+}
